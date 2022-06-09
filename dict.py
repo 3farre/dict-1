0adabcd9af6e203e4a37e850e460e2c0fb15a74c
3farre <farrahane@gmail.com>
2022-06-09 23:27:58 +0200

    lagt till kommentarer i pythonkoden

--- dict.py
+++ dict.py
@@ -8,20 +8,35 @@ conn = psycopg2.connect(
    password="Mddzcj12"
 )
 
+# read_dict: returns the list of all dictionary entries:
+# argument: C - the database connection.
+
 def read_dict(C):
     cur = C.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+
+# add_word: executes to add a word to the list of all dictionary entries:
+# argument: C, word, translation - the database connection, word and translation from table dictionary
+
 def add_word(C, word, translation):
     cur = C.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
+
+# delete_word: executes to deletes the word inserted by finding the id of that word:
+# argument: C, ID - the database connection and id in table.
+
 def delete_word(C, ID):
     cur = C.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
+
+# save_dict: returns the list of all dictionary entries to save:
+# argument: C - the database connection.
+
 def save_dict(C):
     cur = C.cursor()
     cur.execute("COMMIT;")
@@ -32,6 +47,9 @@ list   - list all words
 add    - add a word to dictionary
 delete - delete a word from dictionary
 quit   - exit dictionary""") 
+
+# print_help: returns the list of all commands available:
+# argument: list_of_commands - the function with the list
 
 def print_help():
     print(list_of_commands) 
